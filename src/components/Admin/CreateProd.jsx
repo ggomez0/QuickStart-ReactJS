da4bbd4f098c40ac592a0e306056cb3b81dd893a
Gaspar <gaspargomez2000@outlook.com>
2023-07-31 15:22:33 -0300

    Firestore AddProduct

--- src/components/Admin/CreateProd.jsx
+++ src/components/Admin/CreateProd.jsx
@@ -5,7 +5,7 @@ import { getFirestore, doc, collection, setDoc } from "firebase/firestore";
 
 export function CreateProduct() {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [values, setValues] = useState({
+    const [values, setvalues] = useState({
       name: "",
       price: 0,
       description: "",
@@ -91,8 +91,7 @@ export function CreateProduct() {
               <div class="grid gap-4 mb-4 sm:grid-cols-2">
                     <div>
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <input onChange={(event) =>
-            setvalues((prev) => ({ ...prev, name: event.target.value }))
+                        <input onChange={(event) => setvalues((prev) => ({ ...prev, name: event.target.value }))
           } type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
                     </input></div>
                   
