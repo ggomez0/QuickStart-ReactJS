07d38d8e6f04f9c9ebbd317384298919cad35910
Gaspar <gaspargomez2000@outlook.com>
2023-07-31 17:45:00 -0300

    show products

--- src/components/Inicio/Productos.jsx
+++ src/components/Inicio/Productos.jsx
@@ -1,46 +1,34 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import FirebaseApp from "../../firebase";
+
 export function Productos() {
-  const products = [
-    {
-      id: 1,
-      name: "Earthen Bottle",
-      href: "#",
-      price: "$48",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-      imageAlt:
-        "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-    },
-    {
-      id: 2,
-      name: "Nomad Tumbler",
-      href: "#",
-      price: "$35",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
-      imageAlt:
-        "Olive drab green insulated bottle with flared screw lid and flat top.",
-    },
-    {
-      id: 3,
-      name: "Focus Paper Refill",
-      href: "#",
-      price: "$89",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
-      imageAlt:
-        "Person using a pen to cross a task off a productivity paper card.",
-    },
-    {
-      id: 4,
-      name: "Machined Mechanical Pencil",
-      href: "#",
-      price: "$35",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
-      imageAlt:
-        "Hand holding black machined steel mechanical pencil with brass tip and top.",
-    },
-  ];
+  const firestore = getFirestore(FirebaseApp);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(firestore, "products"));
+        const fetchedData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          // category: doc.data().category,
+          price: doc.data().price,
+          name: doc.data().name,
+          // description: doc.data().description,
+        }));
+        setData(fetchedData);
+        setLoading(false);
+      } 
+      catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
@@ -49,7 +37,7 @@ export function Productos() {
           <h2 className="sr-only">Products</h2>
 
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
+            {data.map((product) => (
               <a key={product.id} href={product.href} className="group">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                   <img
@@ -59,7 +47,7 @@ export function Productos() {
                   />
                 </div>
                 <h3 className="mt-4 text-sm">{product.name}</h3>
-                <p className="mt-1 text-lg font-medium">{product.price}</p>
+                <p className="mt-1 text-lg font-medium">$ {product.price}</p>
               </a>
             ))}
           </div>
