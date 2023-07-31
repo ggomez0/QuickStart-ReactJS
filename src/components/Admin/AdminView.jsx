93d93ff8776ea39c9ef5dbdde0ca10bd43fe4715
ggomez0 <gaspargomez2000@outlook.com>
2023-07-30 23:24:45 -0300

    products

--- src/components/Admin/AdminView.jsx
+++ src/components/Admin/AdminView.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Inicio from "../Inicio/Inicio";
+import CreateProduct from "./CreateProd";
+import ProductsCRUD from "./ProductsCRUD";
 
 export function AdminView() {
 
+
   return (
-    <div className="text-center">
-
-
+    <div className="">
+      <CreateProduct></CreateProduct>
+      <ProductsCRUD></ProductsCRUD>
       <Inicio></Inicio>
     </div>
   );
 }
 
 export default AdminView;
+
