e489cd5614bcc77e3720e102d20d905785a70b93
ggomez0 <gaspargomez2000@outlook.com>
2023-07-30 21:07:30 -0300

    error404

---
+++ src/components/Inicio/Error404.jsx
@@ -0,0 +1,11 @@
+export function Error404() {
+    return (
+        <div className="text-center pt-64">
+           <h1 className="text-white text-2xl">Error 404</h1>
+           <h3>No se encontro la pagina indicada</h3>
+           <br></br>
+        </div>
+    )
+}
+
+export default Error404;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import { Inicio } from "../components/Inicio/Inicio";
 import { Login } from "../components/Login/Login";
 import { Signup } from "../components/Signup/Signup";
 import { Home } from "../components/Home/Home";
+import {Error404} from "../components/Inicio/Error404";
 
 export function MyRoutes() {
   return (
@@ -12,6 +13,7 @@ export function MyRoutes() {
         <Route exact path="/Login" element={<Login />} />
         <Route exact path="/Signup" element={<Signup />} />
         <Route exact path="/Home" element={<Home />} />
+        <Route path="*" Component={Error404} />
       </Routes>
     </Router>
   );
